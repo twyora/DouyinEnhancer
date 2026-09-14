da3188da053af65f38b37a4ad4c8d008e10ce757
twyora <77087199+twyora@users.noreply.github.com>
2026-09-14 21:44:06 +0800

    feat(config): add open() and close() to KVStorage

--- app/src/main/java/io/github/twyora/douyinenhancer/config/KVStorage.kt
+++ app/src/main/java/io/github/twyora/douyinenhancer/config/KVStorage.kt
@@ -1,6 +1,7 @@
 package io.github.twyora.douyinenhancer.config
 
 import kotlinx.coroutines.flow.Flow
+import java.io.File
 
 interface KVStorage {
     fun <T : Any> get(key: String, defValue: T): T
@@ -14,4 +15,15 @@ interface KVStorage {
     fun <T : Any> observe(key: String, defValue: T): Flow<T>
 
     fun clear()
+
+    fun close()
+
+    interface KVFactory {
+        fun open(path: String, name: String): KVStorage
+
+        fun open(path: String): KVStorage {
+            val file = File(path)
+            return open(file.parent ?: ".", file.name)
+        }
+    }
 }
